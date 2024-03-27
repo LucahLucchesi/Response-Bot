782673964a24994001c8b8e84c603d4abbc25ba6
Lucah Lucchesi <lucahlucchesi@gmail.com>
2024-03-26 22:16:57 -0500

    Reply functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const { replyRate, responses} = require('./config.json')
 
 const client = new Client({
     intents: [
@@ -12,6 +13,13 @@ const client = new Client({
 
 client.on('ready', (c) => {
     console.log(`${c.user.tag} is online`);
+});
+
+client.on('messageCreate', (message) => {
+    if(message.author.bot) return;
+    if(message.mentions.has(client.user) || Math.floor(Math.random() * replyRate) == 0){
+        message.reply(responses[Math.floor(Math.random() * responses.length)]);
+    };
 });
 
 client.login(process.env.TOKEN)
